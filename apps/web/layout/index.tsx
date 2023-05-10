edcd44585bab7d27eff883dcb7f25d47ccefb27f
mfelipesilva <silvaafelipe016@gmail.com>
2023-05-10 14:44:43 -0300

    feat: layout and main page changes

--- apps/web/layout/index.tsx
+++ apps/web/layout/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
+import React from "react";
+import NavBar from "../components/NavBar";
 import GlobalStyle from "../styles/global";
+
+import Home from "../pages/home/home";
 
 function Layout() {
   return (
-    <>
+    <div>
       <GlobalStyle />
       <NavBar />
-    </>
-  )
+     
+      <Home />
+    </div>
+  );
 }
 
 export default Layout;
