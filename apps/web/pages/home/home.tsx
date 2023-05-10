6170aefc746b251fd2a769b9ac8da0c25c94c83b
mfelipesilva <silvaafelipe016@gmail.com>
2023-05-10 14:32:43 -0300

    feat: added home page in the project

---
+++ apps/web/pages/home/home.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, HeaderContent, Header, Title, Button, SubTitle, Img } from './styles';
+
+import abstract from "../../assets/abstract.svg";
+
+function Home() {
+  return (
+    <Container>
+      <HeaderContent>
+        <Header>
+          <Title>
+            Have your own <strong>personal <br /> space</strong>, and use it however <br /> you want.
+          </Title>
+
+          <SubTitle>
+            Create an account to save and store everything <br /> you do on the site.
+          </SubTitle>
+
+          <Button>
+            About the site
+          </Button>
+        </Header>
+
+        <Img src={abstract.src} />
+      </HeaderContent>
+    </Container>
+  )
+}
+
+export default Home;
---
+++ apps/web/pages/home/styles.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+
+export const HeaderContent = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 45em 22em;
+`;
+
+export const Title = styled.h1`
+  font-size: 35pt;
+  font-weight: 400;
+  line-height: 1em;
+  
+  strong {
+    font-weight: 600;
+    color: ${({ theme }) => theme.COLORS.SECONDARY};
+  }
+`;
+
+export const Header = styled.div`
+  h1 + h2 {
+    margin: 30px 0 60px 0;
+  }
+`;
+
+export const SubTitle = styled.h2`
+  font-size: 18pt;
+  font-weight: 300;
+  color: #e5e5e5;
+`;
+
+export const Button = styled.button`
+  padding: 1.3em 2em;
+
+  color: #000000;
+  cursor: pointer;
+  font-size: 12pt;
+  font-weight: 600;
+
+  border: 2px solid #000000;
+  box-shadow: 4px 5px;
+  border-radius: 10px;
+  background-color: #eeeeeeff;
+`;
+
+export const Img = styled.img`
+  display: flex;
+  width: 700px;
+  height: 700px;
+`;
