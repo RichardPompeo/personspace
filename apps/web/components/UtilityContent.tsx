d76f6d0a27517a46aadec827841296e60ca730b1
mfelipesilva <silvaafelipe016@gmail.com>
2023-05-16 11:35:19 -0300

    feat: added translate in the buttons texts

--- apps/web/components/UtilityContent.tsx
+++ apps/web/components/UtilityContent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { RiGlobeFill } from "react-icons/ri";
+import { useTranslation } from "react-i18next";
 import {
   Container,
   ButtonSU,
@@ -10,14 +11,16 @@ import {
 } from "../styles/components/UtilityStyles";
 
 export default function UtilityContent() {
+  const { t } = useTranslation();
+
   return (
     <Container>
       <GlobeButton>
         <RiGlobeFill fontSize={22} />
       </GlobeButton>
       <ContentRegistration>
-        <ButtonSI>Sign in</ButtonSI>
-        <ButtonSU>Sign up</ButtonSU>
+        <ButtonSI>{t("utility.buttonsi")}</ButtonSI>
+        <ButtonSU>{t("utility.buttonsu")}</ButtonSU>
       </ContentRegistration>
     </Container>
   );
