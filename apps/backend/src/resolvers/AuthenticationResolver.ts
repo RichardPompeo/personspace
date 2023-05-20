7ed23025773dcd616810c97c85da98598d0743bf
RichardPompeo <pompeorichard@gmail.com>
2023-05-20 18:44:14 -0300

    feat: add sign in mutation

--- apps/backend/src/resolvers/AuthenticationResolver.ts
+++ apps/backend/src/resolvers/AuthenticationResolver.ts
@@ -1,27 +1,38 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 import { UserResolver } from "./UserResolver";
 
 import { SignUpWithEmailAndPasswordInput } from "../dtos/inputs/SignUpWithEmailAndPasswordInput";
-import { SignUpResponseModel } from "../dtos/models/SignUpModel";
+import { SignInWithEmailAndPasswordInput } from "../dtos/inputs/SignInWithEmailAndPassword";
+
+import { AuthenticationResponseModel } from "../dtos/models/AuthenticationResponseModel";
 
 @Resolver()
 export class AuthenticationResolver {
+  auth: Auth;
+
+  constructor() {
+    this.auth = getAuth();
+  }
+
   @Query(() => String)
   async helloWorld() {
     return "Hello World!";
   }
 
-  @Mutation(() => SignUpResponseModel)
+  @Mutation(() => AuthenticationResponseModel)
   signUpWithEmailAndPassword(
     @Arg("input") input: SignUpWithEmailAndPasswordInput
   ) {
-    const auth = getAuth();
-
     const response = new Promise((resolve) => {
-      createUserWithEmailAndPassword(auth, input.email, input.password)
+      createUserWithEmailAndPassword(this.auth, input.email, input.password)
         .then(async (userCredential) => {
           const user = userCredential.user;
 
@@ -29,6 +40,38 @@ export class AuthenticationResolver {
             displayName: input.displayName,
             firebaseId: user.uid,
           });
+
+          resolve({
+            success: true,
+            user: {
+              uid: user.uid,
+              email: user.email,
+              idToken: user.getIdToken(),
+            },
+          });
+        })
+        .catch((error) => {
+          const code = error.code;
+          const message = error.message;
+
+          resolve({
+            success: false,
+            error: { code, message },
+          });
+        });
+    });
+
+    return response;
+  }
+
+  @Mutation(() => AuthenticationResponseModel)
+  signInWithEmailAndPassword(
+    @Arg("input") input: SignInWithEmailAndPasswordInput
+  ) {
+    const response = new Promise((resolve) => {
+      signInWithEmailAndPassword(this.auth, input.email, input.password)
+        .then((userCredential) => {
+          const user = userCredential.user;
 
           resolve({
             success: true,
